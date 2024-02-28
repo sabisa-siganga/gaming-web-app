4a16324d83b9348a4ae49be4ab0f273b421b0e45
Sabisa Siganga <lumisabisa@gmail.com>
2024-02-28 19:18:02 +0200

    Setting up react slick slider

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import styles from './Testimonials.module.scss';
+import './slider.scss';
 import TestimonialCard from './TestimonialCard';
+
+import Slider from 'react-slick';
 
 import user1 from '../../assets/testimonials/user1.png';
 import user2 from '../../assets/testimonials/user2.png';
+import user3 from '../../assets/players/Ellipse 7.png';
+import user4 from '../../assets/players/Ellipse 5.png';
+import user5 from '../../assets/players/Ellipse 9.png';
+
 import LineStrip from '../LineStrip/LineStrip';
 
 const Testimonials = () => {
@@ -22,20 +29,87 @@ const Testimonials = () => {
 				"Another aspect that sets this website apart is its vibrant and passionate community. The forum section provides a platform for gamers from all walks of life to connect, share their experiences, and discuss their favorite titles. I've made valuable friendships and found like-minded individuals who share my enthusiasm for gaming.",
 			image: user2,
 		},
+		{
+			name: 'Cody Fisher',
+			company: 'The Walt Disney',
+			comment:
+				"Another aspect that sets this website apart is its vibrant and passionate community. The forum section provides a platform for gamers from all walks of life to connect, share their experiences, and discuss their favorite titles. I've made valuable friendships and found like-minded individuals who share my enthusiasm for gaming.",
+			image: user3,
+		},
+		{
+			name: 'Robert Fox',
+			company: "L'Oréal",
+			comment:
+				"Another aspect that sets this website apart is its vibrant and passionate community. The forum section provides a platform for gamers from all walks of life to connect, share their experiences, and discuss their favorite titles. I've made valuable friendships and found like-minded individuals who share my enthusiasm for gaming.",
+			image: user4,
+		},
+		{
+			name: 'Wade Warren',
+			company: 'Gillette',
+			comment:
+				"Another aspect that sets this website apart is its vibrant and passionate community. The forum section provides a platform for gamers from all walks of life to connect, share their experiences, and discuss their favorite titles. I've made valuable friendships and found like-minded individuals who share my enthusiasm for gaming.",
+			image: user5,
+		},
+    {
+			name: 'Arlene McCoy',
+			company: "McDonald's",
+			comment:
+				'One of the standout features of this gaming website is its extensive library of game guides and tutorials. It has helped me level up my skills, conquer challenging quests, and discover hidden secrets within games. The guides are comprehensive, easy to follow, and have undoubtedly elevated my gaming performance.',
+			image: user1,
+		},
+    {
+			name: 'Cody Fisher',
+			company: 'The Walt Disney',
+			comment:
+				"Another aspect that sets this website apart is its vibrant and passionate community. The forum section provides a platform for gamers from all walks of life to connect, share their experiences, and discuss their favorite titles. I've made valuable friendships and found like-minded individuals who share my enthusiasm for gaming.",
+			image: user3,
+		},
+    {
+			name: 'Kathryn Murphy',
+			company: 'General Electric',
+			comment:
+				"Another aspect that sets this website apart is its vibrant and passionate community. The forum section provides a platform for gamers from all walks of life to connect, share their experiences, and discuss their favorite titles. I've made valuable friendships and found like-minded individuals who share my enthusiasm for gaming.",
+			image: user2,
+		},
+    {
+			name: 'Wade Warren',
+			company: 'Gillette',
+			comment:
+				"Another aspect that sets this website apart is its vibrant and passionate community. The forum section provides a platform for gamers from all walks of life to connect, share their experiences, and discuss their favorite titles. I've made valuable friendships and found like-minded individuals who share my enthusiasm for gaming.",
+			image: user5,
+		},
+    {
+			name: 'Robert Fox',
+			company: "L'Oréal",
+			comment:
+				"Another aspect that sets this website apart is its vibrant and passionate community. The forum section provides a platform for gamers from all walks of life to connect, share their experiences, and discuss their favorite titles. I've made valuable friendships and found like-minded individuals who share my enthusiasm for gaming.",
+			image: user4,
+		},
 	];
+
+	const settings = {
+		dots: true,
+		infinite: false,
+		arrows: false,
+		speed: 500,
+		slidesToShow: 2,
+		slidesToScroll: 2,
+	};
 
 	return (
 		<section className={styles.testimonialContainer}>
 			<div className={styles.container}>
-				{cards.map((card, index) => (
-					<TestimonialCard
-						key={`testimonial-card-${index}`}
-						name={card.name}
-						comment={card.comment}
-						company={card.company}
-						image={card.image}
-					/>
-				))}
+				<Slider {...settings}>
+					{cards.map((card, index) => (
+						<TestimonialCard
+							key={`testimonial-card-${index}`}
+							name={card.name}
+							comment={card.comment}
+							company={card.company}
+							image={card.image}
+						/>
+					))}
+				</Slider>
 			</div>
 
 			<LineStrip hasBackground className={styles.lineStrip} />
