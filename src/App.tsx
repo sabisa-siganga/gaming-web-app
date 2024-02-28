a069d63584d801d7db7f654a325bdbf9df1138d8
Sabisa-Siganga <lumisabisa@gmail.com>
2024-02-28 15:52:18 +0200

    Implementing the footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import './App.scss';
-import Header from './components/Header/Header';
-import GameCarousel from './components/GameCarousel/GameCarousel';
-import Banner from './components/Banner/Banner';
-import Filters from './components/Filters/Filters';
+import React from "react";
+import "./App.scss";
+import Header from "./components/Header/Header";
+import GameCarousel from "./components/GameCarousel/GameCarousel";
+import Banner from "./components/Banner/Banner";
+import Filters from "./components/Filters/Filters";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-	return (
-		<div className='appContainer'>
-			<div className='sub-container'>
-				<Header />
-				<Banner />
-			</div>
-			<GameCarousel />
+  return (
+    <div className="appContainer">
+      <div className="sub-container">
+        <Header />
+        <Banner />
+      </div>
+      <GameCarousel />
       <Filters />
-		</div>
-	);
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
